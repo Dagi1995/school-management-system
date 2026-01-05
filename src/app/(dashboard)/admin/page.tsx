a9a9b04ee05a3c624de35e089d683c2e354ceed7
Dagi1995 <zereaydagim@gmail.com>
2026-01-05 09:20:00 +0000

    Admin dashboard

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,27 +1,49 @@
-import CountChart from "@/components/CountChart";
-import UseCard from "@/components/UseCard";
+import Announcements from "@/components/Announcements";
+import AttendanceChartContainer from "@/components/AttendanceChartContainer";
+import CountChartContainer from "@/components/CountChartContainer";
+import EventCalendarContainer from "@/components/EventCalendarContainer";
+import FinanceChart from "@/components/FinanceChart";
+import UserCard from "@/components/UserCard";
 
-const AdminPage = () => {
+const AdminPage = ({
+  searchParams,
+}: {
+  searchParams: { [keys: string]: string | undefined };
+}) => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
-      {/* Left Side */}
+      {/* LEFT */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
-        {/* User Statistics Card */}
+        {/* USER CARDS */}
         <div className="flex gap-4 justify-between flex-wrap">
-          <UseCard type="student"></UseCard>
-          <UseCard type="teacher"></UseCard>
-          <UseCard type="parent"></UseCard>
-          <UseCard type="staff"></UseCard>
+          <UserCard type="admin" />
+          <UserCard type="teacher" />
+          <UserCard type="student" />
+          <UserCard type="parent" />
         </div>
-        <div className="flex flex-col gap-4 lg:flex-row">
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
           <div className="w-full lg:w-1/3 h-[450px]">
-            <CountChart></CountChart>
+            <CountChartContainer />
           </div>
-          <div className="w-full lg:w-2/3 h-[450px]"></div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChartContainer />
+          </div>
         </div>
-        <div className=""></div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendarContainer searchParams={searchParams}/>
+        <Announcements />
       </div>
     </div>
   );
 };
+
 export default AdminPage;
